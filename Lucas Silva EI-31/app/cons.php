a6beaf0f6f92971801e9405615aff364d895139f
LucasSilva31 <202313600016@ifba.edu.br>
2026-09-13 13:35:37 +0000

    Correção do cons

--- Lucas Silva EI-31/app/cons.php
+++ Lucas Silva EI-31/app/cons.php
@@ -2,7 +2,7 @@
                $server = "localhost";
                $user = "root";
                $password = "";
-               $db = "AxeTudo";
+               $db = "axetudo";
                $local = "";
                //$local = "https://eun.ifba.edu.br/sistemas/carteira/validar/processar.php?matricula="
                //$local = "http://192.168.1.12/carteira/validar/processar.php?matricula="
