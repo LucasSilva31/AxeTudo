a2af453789561875523b568ab064a732d8d495d9
LucasSilva31 <202313600016@ifba.edu.br>
2026-09-13 13:39:08 +0000

    Correção do salvar_login

--- Lucas Silva EI-31/salvar_login.php
+++ Lucas Silva EI-31/salvar_login.php
@@ -6,7 +6,7 @@ require_once "app/DLL.php";
 extract($_POST);
 
 $senha = md5($senha);
-$consulta = "INSERT INTO logins (id, nome, senha) VALUES (NULL, '$login', '$senha')";
+$consulta = "INSERT INTO logins (id, login, senha) VALUES (NULL, '$login', '$senha')";
 banco($server, $user, $password, $db, $consulta);
 
 $sql = "INSERT INTO logins (login, senha) VALUES ('$login', '$senha')";
